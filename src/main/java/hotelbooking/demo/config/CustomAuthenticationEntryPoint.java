d3197c53d9e175e31ba1abc5c45bef7ecec65f16
Nguyen Duc Thien <nguyenducthienlq1@gmail.com>
2025-12-21 03:48:19 +0700

    fix(auth): fix NPE when handling auth exceptions

--- src/main/java/hotelbooking/demo/config/CustomAuthenticationEntryPoint.java
+++ src/main/java/hotelbooking/demo/config/CustomAuthenticationEntryPoint.java
@@ -28,11 +28,18 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
                          AuthenticationException authException) throws IOException, ServletException {
         this.delegate.commence(request, response, authException);
         response.setContentType("application/json;charset=UTF-8");
+        String errorMessage = "Token không hợp lệ (Hết hạn token, không đúng định dạng,...)";
 
+        if (authException.getCause() != null) {
+            errorMessage = authException.getCause().getMessage();
+        }
+        else if (authException.getMessage() != null) {
+            errorMessage = authException.getMessage();
+        }
         RestResponse<Object> res = new RestResponse<Object>();
         res.setStatusCode(HttpStatus.UNAUTHORIZED.value());
-        res.setError(authException.getCause().getMessage());
-        res.setMessage("Token không hợp lệ (Hết hạn token, không đúng định dạng,...)");
+        res.setError("Unauthorized");
+        res.setMessage(errorMessage);
 
         mapper.writeValue(response.getWriter(), res);
     }
